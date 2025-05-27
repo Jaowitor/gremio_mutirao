a0a3169028df3438449064344abd51badfcbece6
jaowitor <witorjoao430@gmail.com>
2025-05-27 05:18:20 -0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', function(){
-    return 'joao';
-});
-
-Route::prefix()->group(function () {
-
+Route::get('/', function () {
+    return view('welcome');
 });
